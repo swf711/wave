e57973717fe53b390258d15e305f982331987b54
swf711 <33241185+swf711@users.noreply.github.com>
2020-03-18 14:18:14 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import 'bootstrap'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 
 import router from './router'
 
