9b1059a1f0488fa1e27cc8ee183826c2566c91b2
swf711 <33241185+swf711@users.noreply.github.com>
2020-03-18 14:17:49 +0800

    css模組化

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,31 @@
 <template lang="pug">
   #app
-    #nav
-      router-link(to="/") Home
-      | |
-      router-link(to="/about") About
-    router-view
-    .card(style="width: 18rem;")
-      .card-body
-        h5.card-title Card title
-        p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
-        a.btn.btn-primary(href="#") Go somewhere
+    header
+      a.brand(href="/") brian shao
+      nav
+        router-link(to="/") Home
+        | |
+        router-link(to="/about") about
+        router-link(to="/works") works
+        router-link(to="/contact") contact
+    .container
+      router-view
+    footer
+      .mode-select
+        .select-button
+      .copyright
+        span ©2020
+        span brian shao
 </template>
+
+<script>
+  import './assets/css/app.sass'
+
+  export default {}
+</script>
 
 <style lang="sass">
 
-@import "~bootstrap/scss/bootstrap"
-
-#app
-  font-family: Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-
-#nav
-  padding: 30px
-  a
-    font-weight: bold
-    color: #2c3e50
-    &.router-link-exact-active
-      color: #42b983
+  
       
 </style>
