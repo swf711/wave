23130b288330d703d475128d9655a561106d447c
swf711 <33241185+swf711@users.noreply.github.com>
2020-03-31 18:21:05 +0800

    about page update

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,26 @@
 	section.about
 		img.about-img(src="../assets/images/profile-image.jpg")
 		img.about-img-bd(src="../assets/images/profile-image-border.png")
+		.content
+			h1 Hi I’m 
+				span Brian Shao 
+				| !
+			h3 A web designer based in Taipei, Taiwan.<br>
+				| Focused on digital interactive experiences.<br>
+				| Love web & effect designing,<br>
+				| always trying to make <br>
+				| intresting interactive web page.
+			.title EXPERIENCE
+			.exp 輔仁大學資訊管理系畢業<br>
+				| BACHELOR IN INFORMATION MANAGEMENT
+				.company 2012 - 2016｜Fu Jen Catholic Unuversity
+			.exp 網路行銷企劃<br>
+				| INTERNET MARKETING PERSONNEL
+				.company 2016 - 2017｜你最大行銷公司
+			.exp 網頁設計師<br>
+				| WEB DESIGNER
+				.company 2017 - 2020｜佳音英語總公司
+			.title SKILLS
 		//- HelloWorld(msg="Welcome to Your Vue.js App")
 </template>
 
@@ -10,9 +30,9 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'About',
-  components: {
-    HelloWorld
-  }
+  // name: 'About',
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
