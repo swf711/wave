6d367d2388ed7296210599884f88b1733e540cdd
swf711 <33241185+swf711@users.noreply.github.com>
2020-05-27 00:49:04 +0800

    Update wave-cursor.vue

--- src/components/wave-cursor.vue
+++ src/components/wave-cursor.vue
@@ -12,14 +12,14 @@ export default {
     return {
       x: 0,
       y: 0,
-      hover: false,
+      hover: false
     }
   },
   mounted(){
     document.addEventListener('mousemove', this.moveCursor);
-
+    
     const vm = this;
-    const hoverTargets = document.querySelectorAll('#app a, #app label');
+    var hoverTargets = document.querySelectorAll('#app a, #app label');
     for (let target of hoverTargets) {
       target.addEventListener('mouseover', function(){
         vm.hover = true;
@@ -28,6 +28,7 @@ export default {
         vm.hover = false;
       });
     }
+    console.log(hoverTargets.length);
   },
   computed: {
     cursorPosition(){
@@ -40,20 +41,21 @@ export default {
       this.y = e.clientY;
     }
   },
-  // watch:{
-  //   $route (to, from){
-  //     const vm = this;
-  //     const hoverTargets = document.querySelectorAll('#app a, #app label');
-  //     for (let target of hoverTargets) {
-  //       target.addEventListener('mouseover', function(){
-  //         vm.hover = true;
-  //       });
-  //       target.addEventListener('mouseout', function(){
-  //         vm.hover = false;
-  //       });
-  //       console.log(hoverTargets);
-  //     }
-  //   }
-  // }
+  watch:{
+    $route (to, from){
+      const vm = this;
+      var hoverTargets = document.querySelectorAll('#app a, #app label');
+      for (let target of hoverTargets) {
+        target.addEventListener('mouseover', function(){
+          vm.hover = true;
+        });
+        target.addEventListener('mouseout', function(){
+          vm.hover = false;
+        });
+      }
+      console.log(hoverTargets.length);
+      console.log(hoverTargets);
+    }
+  }
 }
 </script>
